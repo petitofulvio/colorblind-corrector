a00b259b19c74d73bfdb8fdc33e969f45b3be54f
Fulvio Petito <petito.fulvio@gmail.com>
2024-12-06 22:38:41 +0100

    First working version of Main.java with modified Maven setup

---
+++ src/main/java/com/fulviopetito/Main.java
@@ -0,0 +1,12 @@
+package com.fulviopetito;
+/**
+ * Main class
+ */
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("nothing to see here");
+    }
+}
